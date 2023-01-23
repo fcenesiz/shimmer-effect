3726748b182199e94c58c4390701cec3654a0d96
Ahmet Fatih Çenesiz <fcenesiz@gmail.com>
2023-01-23 14:19:32 +0300

    shimmer effect is ready with an example

--- app/src/main/java/com/fcenesiz/shimmereffect/MainActivity.kt
+++ app/src/main/java/com/fcenesiz/shimmereffect/MainActivity.kt
@@ -3,21 +3,65 @@ package com.fcenesiz.shimmereffect
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.fcenesiz.shimmereffect.ui.theme.ShimmerEffectTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ShimmerEffectTheme {
-
+                var isLoading by remember {
+                    mutableStateOf(true)
+                }
+                LaunchedEffect(key1 = true){
+                    delay(5000)
+                    isLoading = false
+                }
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize()
+                ){
+                    items(20){
+                        ShimmerListItem(
+                            isLoading = isLoading,
+                            contentAfterLoading = {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(16.dp)
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Default.Home,
+                                        contentDescription = null,
+                                        modifier = Modifier.size(100.dp)
+                                    )
+                                    Spacer(modifier = Modifier.width(16.dp))
+                                    Text(
+                                        text = "This is a long text to show that our shimmer display" +
+                                                "is looking perfectly fine."
+                                    )
+                                }
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        )
+                    }
+                }
             }
         }
     }
